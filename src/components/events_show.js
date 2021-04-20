0de3dd2f812c163e23060c63ddd1e6b6b64e24d0
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-04-20 20:28:23 +0900

    implement eventsShow

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -12,6 +12,11 @@ class EventsShow extends Component {
     this.onDeleteClick = this.onDeleteClick.bind(this)
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params
+    if (id) this.props.getEvent(id)
+  }
+
   renderField(field) {
     const { input, label, type, meta: { touched, error } } = field
 
@@ -28,7 +33,7 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-    // await this.props.postEvents(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
@@ -57,8 +62,14 @@ const validate = values => {
 
   return errors
 }
-const mapDispatchToProps = ({ deleteEvent })
 
-export default connect(null, mapDispatchToProps)(
-  reduxForm({ validate, form: 'eventShowForm' })(EventsShow)
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id]
+  return { initialValues: event, event }
+}
+
+const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventShowForm', enableReinitialize: true })(EventsShow)
 )
